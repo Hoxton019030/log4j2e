d1087ad3a7b1cb386bfbd26fd88669b2b945af5c
DESKTOP-RBMVNA4\Hoxton <hoxton.hsu@eunodata.com>
2022-08-16 14:40:22 +0800

    2123123

--- src/main/java/Log4j2Test.java
+++ src/main/java/Log4j2Test.java
@@ -5,15 +5,20 @@ import org.junit.Test;
 public class Log4j2Test {
     // 定義日誌記錄器物件
     public static final Logger LOGGER = LogManager.getLogger(Log4j2Test.class);
+
     // 快速入門
     @Test
-    public void testQuick() throws Exception {
-        // 日誌訊息輸出
-        LOGGER.fatal("fatal");
-        LOGGER.error("error");
-        LOGGER.warn("warn");
-        LOGGER.info("info");
-        LOGGER.debug("debug");
-        LOGGER.trace("trace");
+    public void testQuick() {
+        long startTime = System.nanoTime();
+        // 日誌輸出
+        for (int i = 0; i <1000000; i++) {
+            LOGGER.error("error");
+            LOGGER.warn("warning");
+            LOGGER.info("info");    // 預設的日誌級別資訊
+            LOGGER.debug("debug");
+            LOGGER.trace("trace");  // 追蹤資訊
+        }
+        long endTime = System.nanoTime();
+        System.out.println("執行時間" + (endTime - startTime) / 1000000 + "毫秒");
     }
 }
